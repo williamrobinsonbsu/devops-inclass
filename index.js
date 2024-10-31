e7fecb8c238acbaa5b73317e8c400bf7de520997
williamrobinsonbsu <143113909+williamrobinsonbsu@users.noreply.github.com>
2024-10-31 14:17:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+exports.fibonacci = function(n) {
+    if(n==0 || n==1)
+        return n;
+    return this.fibonacci(n-1) + this.fibonacci(n-2);
+}
---
+++ test/test.js
@@ -0,0 +1,14 @@
+var main = require('../index');
+var assert = require('assert');
+describe('Regular fibonacci value', function() {
+    it('should return 5 for Fib(5)', function() {
+        assert.strictEqual(main.fibonacci(5), 5);
+      });
+});
+
+describe('First two terms', function() {
+  it('should return 0 and 1 for first two terms', function() {
+      assert.strictEqual(main.fibonacci(0), 0);
+      assert.strictEqual(main.fibonacci(1), 1);
+    });
+});
